13eb39b3d5277cb59295f66d61d4f03f152c1e22
madudamattos <mariaeduardanmattos@gmail.com>
2023-11-16 10:44:08 -0300

    primeiras modificacoes

---
+++ Respostas/tMenu.c
@@ -0,0 +1,38 @@
+
+void MenuCaminhoBancoDados(){
+    printf("################################################\nDIGITE O CAMINHO DO BANCO DE DADOS:\n################################################\n");
+}
+
+void MenuAcessoMiniSade(){
+    printf("######################## ACESSO MINI-SADE ######################\nDIGITE SEU LOGIN:\nDIGITE SUA SENHA:\n###############################################################\n");
+}
+
+void MenuPrincipalAdmin(){
+    printf("####################### MENU PRINCIPAL #########################\nESCOLHA UMA OPCAO:\n\t(1) CADASTRAR SECRETARIO\n\t(2) CADASTRAR MEDICO\n\t(3) CADASTRAR PACIENTE\n\t(4) REALIZAR CONSULTA\n\t(5) BUSCAR PACIENTES\n\t(6) RELATORIO GERAL\n\t(7) FILA DE IMPRESSAO\n\t(8) FINALIZAR O PROGRAMA\n###############################################################\n");
+}
+
+void MenuPrincipalUser(){
+    printf("####################### MENU PRINCIPAL #########################\nESCOLHA UMA OPCAO:\n\t(2) CADASTRAR MEDICO\n\t(3) CADASTRAR PACIENTE\n\t(5) BUSCAR PACIENTES\n\t(6) RELATORIO GERAL\n\t(7) FILA DE IMPRESSAO\n\t(8) FINALIZAR O PROGRAMA\n###############################################################\n");
+}
+
+void MenuPrincipalMedico(){
+    printf("####################### MENU PRINCIPAL #########################\nESCOLHA UMA OPCAO:\n\t(4) REALIZAR CONSULTA\n\t(5) BUSCAR PACIENTES\n\t(8) FINALIZAR O PROGRAMA\n###############################################################\n");
+}
+
+void MenuConsultaMedica(){
+    printf("#################### CONSULTA MEDICA #######################\nESCOLHA UMA OPCAO:\n\t(1) CADASTRAR LESAO\n\t(2) GERAR RECEITA MEDICA\n\t(3) SOLICITACAO DE BIOPSIA\n\t(4) ENCAMINHAMENTO\n\t(5) ENCERRAR CONSULTA\n############################################################\n");
+}
+
+void MenuFilaImpressao(){
+    printf("################### FILA DE IMPRESSAO MEDICA #####################\nESCOLHA UMA OPCAO:\n\t(1) EXECUTAR FILA DE IMPRESSAO\n\t(2) RETORNAR AO MENU PRINCIPAL\n############################################################\n");
+}
+
+void MenuBuscaPacientes(){
+    printf("#################### BUSCAR PACIENTES #######################\nNOME DO PACIENTE:\nDATA DA CONSULTA:\nDIAGNOSTICO:\n############################################################\n");
+}
+
+
+
+
+
+
